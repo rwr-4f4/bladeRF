b93d42575606705298a625003c515bfd718475ff
Jon Szymaniak <jon.szymaniak@gmail.com>
2014-09-25 15:23:53 -0400

    libbladeRF: [WIP] Added gmsk_burst to list of tests in help text

--- host/libraries/libbladeRF/tests/test_timestamps/src/main.c
+++ host/libraries/libbladeRF/tests/test_timestamps/src/main.c
@@ -113,6 +113,8 @@ static void usage(const char *argv0)
     printf("         rx_scheduled         Perform reads at specific timestamps.\n");
     printf("         tx_onoff             Transmits ON-OFF bursts.\n");
     printf("                                Requires external verification.\n");
+    printf("         tx_gmsk_bursts       Transmits GMSK bursts.\n");
+    printf("                                Requires external verification.\n");
     printf("         loopback_onoff       Transmits ON-OFF bursts which are verified\n");
     printf("                                via baseband loopback to the RX module.\n");
     printf("\n");
