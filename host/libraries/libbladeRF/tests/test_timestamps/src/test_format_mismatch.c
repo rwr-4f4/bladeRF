7bc22a2ae4e8c70c869cbb5ee4cf02c7d3a7609a
Jon Szymaniak <jon.szymaniak@gmail.com>
2014-09-26 15:23:22 -0400

    libbladeRF: [WIP] Adding async i/f to format mismatch test

--- host/libraries/libbladeRF/tests/test_timestamps/src/test_format_mismatch.c
+++ host/libraries/libbladeRF/tests/test_timestamps/src/test_format_mismatch.c
@@ -40,7 +40,8 @@ const struct interfaces {
     bool rx_sync;
     bool tx_sync;
 } ifs[] = {
-    { true, true },
+    { true, true  },
+    { true, false },
 };
 
 const struct test_case {
@@ -53,6 +54,103 @@ const struct test_case {
     { BLADERF_FORMAT_SC16_Q11_META, BLADERF_FORMAT_SC16_Q11, BLADERF_ERR_INVAL  },
     { BLADERF_FORMAT_SC16_Q11_META, BLADERF_FORMAT_SC16_Q11_META, 0 },
 };
+
+typedef enum {
+   TASK_UNINITIALIZED,
+   TASK_RUNNING,
+   TASK_SHUTDOWN_REQUESTED,
+   TASK_STOPPED,
+} task_state;
+
+static struct async_task
+{
+    int exit_status;
+    struct bladerf *dev;
+    bladerf_module module;
+    bladerf_format fmt;
+    struct app_params *p;
+    task_state state;
+
+    pthread_t thread;
+    pthread_mutex_t lock;
+} rx_task, tx_task;
+
+void *callback(struct bladerf *dev, struct bladerf_stream *stream,
+               struct bladerf_metadata *meta, void *samples, size_t num_samples,
+               void *user_data)
+
+{
+    struct async_task *t = (struct async_task *) user_data;
+    void *ret = NULL;
+
+
+    pthread_mutex_lock(&t->lock);
+
+    printf("State is %d in callback.\n", t->state);
+
+    switch (t->state) {
+        case TASK_UNINITIALIZED:
+            t->state = TASK_RUNNING;
+            ret = samples;
+            break;
+
+        case TASK_RUNNING:
+            ret = samples;
+            break;
+
+        case TASK_SHUTDOWN_REQUESTED:
+            ret = BLADERF_STREAM_SHUTDOWN;
+            break;
+
+        default:
+            fprintf(stderr, "Unexpected task state: %d\n", t->state);
+            ret = BLADERF_STREAM_SHUTDOWN;
+    }
+
+    pthread_mutex_unlock(&t->lock);
+    return ret;
+}
+
+static void *stream_task(void *arg)
+{
+    int status;
+    struct bladerf_stream *stream;
+    struct async_task *t = (struct async_task *) arg;
+
+    status = bladerf_init_stream(&stream, t->dev, callback, NULL, 16, t->fmt,
+                                 t->p->buf_size, t->p->num_xfers, t);
+
+    if (status != 0) {
+        t->exit_status = BLADERF_ERR_UNEXPECTED;
+        fprintf(stderr, "Failed to initialized %s stream: %s\n",
+                (t->module == BLADERF_MODULE_RX) ? "RX" : "TX",
+                bladerf_strerror(status));
+        goto out;
+    }
+
+    status = bladerf_enable_module(t->dev, t->module, true);
+    if (status != 0) {
+        fprintf(stderr, "Failed to enable %s module: %s\n",
+                t->module == BLADERF_MODULE_RX ? "RX" : "TX",
+                bladerf_strerror(status));
+        goto out;
+    }
+
+    printf("Initialized stream. Starting it...\n");
+
+    status = bladerf_stream(stream, t->module);
+    bladerf_deinit_stream(stream);
+
+out:
+    pthread_mutex_lock(&t->lock);
+    t->state = TASK_STOPPED;
+    t->exit_status = status;
+    pthread_mutex_unlock(&t->lock);
+
+    printf("Task ending.\n");
+
+    return NULL;
+}
 
 static int enable_modules(struct bladerf *dev, bool enable)
 {
@@ -81,29 +179,130 @@ static int init_if(struct bladerf *dev, struct app_params *p, bladerf_module m,
                    bool sync, bladerf_format fmt, int exp_status)
 {
     int status;
+    struct async_task *t;
 
     if (sync) {
         status = bladerf_sync_config(dev, m, fmt, p->num_buffers, p->buf_size,
                                      p->num_xfers, p->timeout_ms);
-    } else {
-        status = -1; /* TODO */
-    }
 
-    if (status != exp_status) {
-        fprintf(stderr,
-                "Did not receive expected status when configuring %s: %s\n",
-                m == BLADERF_MODULE_RX ? "RX" : "TX", bladerf_strerror(status));
-        status = -1;
+        if (status != exp_status) {
+            fprintf(stderr,
+                    "Uexpected status when configuring sync %s: %s\n",
+                    m == BLADERF_MODULE_RX ? "RX" : "TX",
+                    bladerf_strerror(status));
+            status = -1;
+        } else {
+            status = 0;
+        }
     } else {
-        status = 0;
+
+        task_state state;
+
+        switch (m) {
+            case BLADERF_MODULE_RX:
+                t = &rx_task;
+                break;
+
+            case BLADERF_MODULE_TX:
+                t = &tx_task;
+                break;
+
+            default:
+                fprintf(stderr, "Invalid module: %d\n", m);
+                return -1;
+        }
+
+        t->fmt = fmt;
+        t->state = state = TASK_UNINITIALIZED;
+
+        status = pthread_create(&t->thread, NULL, stream_task, t);
+        if (status != 0) {
+            fprintf(stderr, "Failed to launch %s task\n",
+                    m == BLADERF_MODULE_RX ? "RX" : "TX");
+            return status;
+        }
+
+        printf("Started task\n");
+
+        do {
+            usleep(25000);
+            pthread_mutex_lock(&t->lock);
+            state = t->state;
+            pthread_mutex_unlock(&t->lock);
+        } while (state == TASK_UNINITIALIZED);
+
+        printf("Task is now in state %d\n", state);
+
+        /* This may have already stopped if an invalid configuration was
+         * detected. */
+        if (state == TASK_STOPPED) {
+            pthread_join(t->thread, NULL);
+            if (t->exit_status != exp_status) {
+                fprintf(stderr,
+                        "Uexpected status when configuring async %s: %s\n",
+                        m == BLADERF_MODULE_RX ? "RX" : "TX",
+                        bladerf_strerror(status));
+                status = -1;
+            } else {
+                status = 0;
+            }
+
+            printf("Task exited in init with %d\n", t->exit_status);
+        }
     }
 
     return status;
 }
 
-static int deinit_ifs(struct bladerf *dev, const struct interfaces *ifs)
+static int deinit_if(struct bladerf *dev, bladerf_module m, bool sync,
+                     int exp_status)
 {
-    return 0;
+    int status = 0;
+
+    /* Nothing to do for sync i/f -- errors are reported during the config */
+    if (!sync) {
+        struct async_task *t;
+        task_state state;
+
+        switch (m) {
+            case BLADERF_MODULE_RX:
+                t = &rx_task;
+                break;
+
+            case BLADERF_MODULE_TX:
+                t = &tx_task;
+                break;
+
+            default:
+                fprintf(stderr, "Invalid module: %d\n", m);
+                return -1;
+        }
+
+        pthread_mutex_lock(&t->lock);
+        t->state = state = TASK_SHUTDOWN_REQUESTED;
+        pthread_mutex_unlock(&t->lock);
+
+        do {
+            usleep(25000);
+            pthread_mutex_lock(&t->lock);
+            state = t->state;
+            pthread_mutex_unlock(&t->lock);
+        } while (state == TASK_SHUTDOWN_REQUESTED);
+
+        pthread_join(t->thread, NULL);
+
+        if (t->exit_status != exp_status) {
+            fprintf(stderr,
+                    "Uexpected status when deinitializing async %s: %s\n",
+                    m == BLADERF_MODULE_RX ? "RX" : "TX",
+                    bladerf_strerror(t->exit_status));
+            status = -1;
+        } else {
+            status = 0;
+        }
+    }
+
+    return status;
 }
 
 static void print_test_info(int n, const struct interfaces *i,
@@ -119,23 +318,52 @@ static void print_test_info(int n, const struct interfaces *i,
                             "SC16 Q11" : "SC16 Q11 + Metadata");
 }
 
+static void init_task_info(struct bladerf *dev, struct app_params *p,
+                           bladerf_module m, struct async_task *t)
+{
+    memset(t, 0, sizeof(t[0]));
+
+    t->state = TASK_UNINITIALIZED;
+    t->dev = dev;
+    t->module = m;
+    t->p = p;
+
+    pthread_mutex_init(&t->lock, NULL);
+}
+
 int test_fn_format_mismatch(struct bladerf *dev, struct app_params *p)
 {
     int status;
     size_t i, j;
+    bladerf_loopback lb_backup;
+
+    init_task_info(dev, p, BLADERF_MODULE_RX, &rx_task);
+    init_task_info(dev, p, BLADERF_MODULE_RX, &tx_task);
+
+    status = bladerf_get_loopback(dev, &lb_backup);
+    if (status != 0) {
+        fprintf(stderr, "Failed to get current loopback mode.\n");
+        return status;
+    }
+
+    status = bladerf_set_loopback(dev, BLADERF_LB_BB_TXVGA1_RXVGA2);
+    if (status != 0) {
+        fprintf(stderr, "Failed to set loopback mode.\n");
+        return status;
+    }
 
     for (i = 0; i < ARRAY_SIZE(ifs); i++) {
 
         for (j = 0; j < ARRAY_SIZE(tests); j++) {
             print_test_info(i * ARRAY_SIZE(tests) + j, &ifs[i], &tests[j]);
 
-            /* Init RX first, then TX */
-            printf("RX -> TX...");
-
             status = enable_modules(dev, false);
             if (status != 0) {
                 goto fail;
             }
+
+            /* Init RX first, then TX */
+            printf("RX -> TX...");
 
             status = init_if(dev, p, BLADERF_MODULE_RX, ifs[i].rx_sync,
                              tests[j].rx_fmt, 0);
@@ -150,7 +378,18 @@ int test_fn_format_mismatch(struct bladerf *dev, struct app_params *p)
                 goto fail;
             }
 
-            status = deinit_ifs(dev, &ifs[i]);
+            status = deinit_if(dev, BLADERF_MODULE_RX, ifs[i].rx_sync, 0);
+            if (status != 0) {
+                goto fail;
+            }
+
+            status = deinit_if(dev, BLADERF_MODULE_TX, ifs[i].tx_sync,
+                               tests[j].exp_status);
+            if (status != 0) {
+                goto fail;
+            }
+
+            status = enable_modules(dev, false);
             if (status != 0) {
                 goto fail;
             }
@@ -159,11 +398,6 @@ int test_fn_format_mismatch(struct bladerf *dev, struct app_params *p)
 
             /* Init TX first, then RX */
             printf("TX -> RX...");
-
-            status = enable_modules(dev, false);
-            if (status != 0) {
-                goto fail;
-            }
 
             status = init_if(dev, p, BLADERF_MODULE_TX, ifs[i].tx_sync,
                              tests[j].tx_fmt, 0);
@@ -178,7 +412,24 @@ int test_fn_format_mismatch(struct bladerf *dev, struct app_params *p)
                 goto fail;
             }
 
+            status = deinit_if(dev, BLADERF_MODULE_TX, ifs[i].tx_sync, 0);
+            if (status != 0) {
+                goto fail;
+            }
+
+            status = deinit_if(dev, BLADERF_MODULE_RX, ifs[i].rx_sync,
+                               tests[j].exp_status);
+            if (status != 0) {
+                goto fail;
+            }
+
+            status = enable_modules(dev, false);
+            if (status != 0) {
+                goto fail;
+            }
+
             printf("Pass.\n");
+
         }
     }
 
@@ -186,7 +437,11 @@ fail:
     if (status != 0) {
         printf("Fail.\n");
     }
-
     printf("\n");
+
+    if (bladerf_set_loopback(dev, lb_backup) != 0) {
+        fprintf(stderr, "Failed to restore loopback settings.\n");
+    }
+
     return status;
 }
